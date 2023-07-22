064d56a24aee27830356d5217c81e54a3970a799
Samuel Poirier <samuel.poirier@beslogic.com>
2023-07-22 15:35:03 -0400

    Implement two identical APIs in .net core 7 and go

---
+++ README.md
@@ -0,0 +1,14 @@
+# API Size Comparison
+
+## To build and run both APIs
+
+`docker compose up --build`
+
+## .NET 7 Minimal API vs Go API
+
+|REPOSITORY|DOCKER IMAGE SIZE|IDLE RAM USAGE|
+|-|-|-|
+|.NET 7 Minimal API| 212 MB| 17.81 MB|
+|Go API| 6.82 MB| 2.7 MB|
+
+![img](./img/API%20comparisons.png)
---
+++ golang-api/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"math"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Response struct {
+	Date         string `json:"date"`
+	TemperatureC int    `json:"temperatureC"`
+	Summary      string `json:"summary"`
+	TemperatureF int    `json:"temperatureF"`
+}
+
+type WeatherForecast struct {
+	TemperatureC int
+	Date         time.Time
+	Summary      string
+}
+
+func (forecast *WeatherForecast) TemperatureF() int {
+	return int(math.Round((32 + float64(forecast.TemperatureC)/0.5556)))
+}
+
+func (forecast *WeatherForecast) ToResponse() Response {
+	return Response{
+		TemperatureC: forecast.TemperatureC,
+		Date:         forecast.Date.Format("2006-01-02"),
+		Summary:      forecast.Summary,
+		TemperatureF: forecast.TemperatureF(),
+	}
+}
+
+func handleWeatherForecast(w http.ResponseWriter, req *http.Request) {
+	summaries := [...]string{
+		"Freezing",
+		"Bracing",
+		"Chilly",
+		"Cool",
+		"Mild",
+		"Warm",
+		"Balmy",
+		"Hot",
+		"Sweltering",
+		"Scorching",
+	}
+
+	forecasts := [5]WeatherForecast{}
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < len(forecasts); i++ {
+		forecasts[i] = WeatherForecast{
+			TemperatureC: rand.Intn(55-20) + 20,
+			Date:         time.Now().AddDate(0, 0, i),
+			Summary:      summaries[rand.Intn(len(summaries))],
+		}
+	}
+
+	respondJson(w, forecasts)
+}
+
+func respondJson(w http.ResponseWriter, data [5]WeatherForecast) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	response := [5]Response{}
+
+	for i, element := range data {
+		response[i] = element.ToResponse()
+	}
+
+	json.NewEncoder(w).Encode(response)
+
+}
+
+func main() {
+
+	http.HandleFunc("/weatherforecast", handleWeatherForecast)
+
+	http.ListenAndServe(":8090", nil)
+}
